c2462310d1e552fb9da69cc745323225078ce68b
Ali <Ali.qurbani2003@outlook.com>
2021-04-20 10:09:50 +0200

    no

--- content/reserveren.php
+++ content/reserveren.php
@@ -16,28 +16,6 @@
 
 
 
-<<<<<<< HEAD:content/reserveren.php
-<header id="navbar">
-        <nav class="navbar-container container">
-          <a href="/" class="home-link">
-            <div class="navbar-logo"></div>
-          </a>
-          <button type="button" class="navbar-toggle" aria-label="Open navigation menu">
-              <span class="icon-bar">test1</span>
-              <span class="icon-bar">test2</span>
-              <span class="icon-bar">test3</span>
-          </button>
-          <div class="navbar-menu">
-            <ul class="navbar-links">
-            <li class="navbar-item"><a class="navbar-link" href="index.php?content=/content/menu">Menu</a></li>
-              <li class="navbar-item"><a class="navbar-link" href="index.php?content=/content/reserveren">Reserveren</a></li>
-              <li class="navbar-item"><a class="navbar-link" href="index.php?content=/content/contact">Contact</a></li>
-            </ul>
-          </div>
-        </nav>
-    </header>
-=======
->>>>>>> 60a82aa1554dea7bcc04466f71a13260cf29ea72:reserveren.php
 
 
 
