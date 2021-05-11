ae62337d8ceaed91807c6d5e95378928f349c533
Ali <Ali.qurbani2003@outlook.com>
2021-05-11 11:38:48 +0200

    added stuff to home.php and navbar.php

---
+++ home.php
@@ -0,0 +1,27 @@
+<style>
+      .navbar {
+        animation: navbar-entrance 1s;
+      }
+
+      @keyframes navbar-entrance {
+        0%   {top:-50px;}
+        100% {top:0px;}
+      }
+    </style> 
+    <title>George Hollywood | Home</title>
+</head>
+<img src="./img/background.jpg" alt="Duivendrechtse Kade 85" width="100%"> 
+
+<div id="content">
+<p>GEORGE likes to welcome you back home. We’ve missed you.</p>
+<p>And we know you’ve missed us too.</p>
+
+<p>Can’t wait to dine al fresco on one of our sunny terraces? Sip a </p>
+<p>rosé and enjoy our most-loved Egg Florentine, Caesar Salad, </p>
+<p>Steak Frites or Pizza Tuna?</p>
+
+<p>On sunny days we have valet parking for a fee of €12,50 during</p>
+<p>lunch and €15 during dinner.</p>
+
+<h4>Then make sure to make an reservation.</h4>
+</div>
--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>George Hollywood | Home</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
